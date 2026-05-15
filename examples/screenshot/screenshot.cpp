0d7b79ec2f3521abeb9ec849835bf1c4efb82ae0
luzehui <luzhluzh0422@gmail.com>
2026-05-15 17:25:30 +0800

    Add HarmonyOS support for screenshot example

--- examples/screenshot/screenshot.cpp
+++ examples/screenshot/screenshot.cpp
@@ -407,7 +407,12 @@ public:
 	{
 		if (overlay->header("Functions")) {
 			if (overlay->button("Take screenshot")) {
+#if defined (VK_USE_PLATFORM_OHOS)
+                std::string screenshotPath = vks::OHOS::filesDir + "/screenshot.ppm";
+                saveScreenshot(screenshotPath.c_str());
+#else
 				saveScreenshot("screenshot.ppm");
+#endif
 			}
 			if (screenshotSaved) {
 				overlay->text("Screenshot saved as screenshot.ppm");
